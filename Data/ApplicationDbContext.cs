05162d4a510513bc8b827741934af4934d1b2ff2
robust-crypto <78141405+robust-crypto@users.noreply.github.com>
2025-11-21 18:01:41 +0200

    Updated DBContext File with values for invoice

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 using ContractMonthlyClaimSystem.Models;
 
 namespace ContractMonthlyClaimSystem.Data
@@ -12,6 +11,55 @@ namespace ContractMonthlyClaimSystem.Data
         {
         }
 
+        // Existing
         public DbSet<LecturerClaim> LecturerClaims { get; set; }
+
+        // PART 3 ADDITIONS
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure LecturerClaim
+            modelBuilder.Entity<LecturerClaim>()
+                .Property(lc => lc.HoursWorked)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<LecturerClaim>()
+                .Property(lc => lc.HourlyRate)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<LecturerClaim>()
+                .Property(lc => lc.TotalAmount)
+                .HasPrecision(18, 2);
+
+            // Configure Lecturer
+            modelBuilder.Entity<Lecturer>()
+                .Property(l => l.DefaultHourlyRate)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Lecturer>()
+                .HasIndex(l => l.Email)
+                .IsUnique();
+
+            // Configure Invoice
+            modelBuilder.Entity<Invoice>()
+                .Property(i => i.TotalAmount)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(i => i.TaxAmount)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(i => i.NetAmount)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .HasIndex(i => i.InvoiceNumber)
+                .IsUnique();
+        }
     }
 }
